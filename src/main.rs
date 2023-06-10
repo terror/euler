df4abc5dacd207d7b5f7bcd845b543534ee3e827
Liam <liam@scalzulli.com>
2023-06-10 00:44:05 -0400

    Parse mcgill course information

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use {
+  anyhow::anyhow,
   chatgpt::prelude::*,
   dotenv::dotenv,
   log::info,
   rand::seq::SliceRandom,
+  regex::Regex,
+  scraper::{ElementRef, Html, Selector},
   serenity::{
     async_trait,
     framework::standard::{
@@ -14,6 +17,64 @@ use {
   },
   std::{env, process},
 };
+
+const MCGILL_TERM: &str = "2023-2024";
+
+pub(crate) trait Select<'a> {
+  fn select_single(&self, selector: &str) -> Result<ElementRef<'a>>;
+  fn try_select_single(&self, selectors: Vec<&str>) -> Result<ElementRef<'a>>;
+  fn select_optional(&self, selector: &str) -> Result<Option<ElementRef<'a>>>;
+  fn select_many(&self, selector: &str) -> Result<Vec<ElementRef<'a>>>;
+}
+
+impl<'a> Select<'a> for ElementRef<'a> {
+  fn select_single(&self, selector: &str) -> Result<ElementRef<'a>> {
+    self
+      .select(
+        &Selector::parse(selector)
+          .map_err(|error| anyhow!("Failed to parse selector: {:?}", error))?,
+      )
+      .next()
+      .ok_or_else(|| anyhow!("Failed to select element"))
+  }
+
+  fn try_select_single(&self, selectors: Vec<&str>) -> Result<ElementRef<'a>> {
+    selectors
+      .iter()
+      .map(|selector| self.select_single(selector))
+      .find(|result| result.is_ok())
+      .unwrap_or_else(|| {
+        Err(anyhow!(
+          "Failed to select element with selectors: {:?}",
+          selectors
+        ))
+      })
+  }
+
+  fn select_optional(&self, selector: &str) -> Result<Option<ElementRef<'a>>> {
+    Ok(
+      self
+        .select(
+          &Selector::parse(selector).map_err(|error| {
+            anyhow!("Failed to parse selector: {:?}", error)
+          })?,
+        )
+        .next(),
+    )
+  }
+
+  fn select_many(&self, selector: &str) -> Result<Vec<ElementRef<'a>>> {
+    Ok(
+      self
+        .select(
+          &Selector::parse(selector).map_err(|error| {
+            anyhow!("Failed to parse selector: {:?}", error)
+          })?,
+        )
+        .collect(),
+    )
+  }
+}
 
 struct Handler;
 
@@ -47,13 +108,178 @@ async fn ai(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
   Ok(())
 }
 
+struct CourseHtmlWrapper(Html);
+
+unsafe impl Send for CourseHtmlWrapper {}
+
+impl CourseHtmlWrapper {
+  fn title(&self) -> Result<String> {
+    Ok(
+      self
+        .0
+        .root_element()
+        .select_single("h1[id='page-title']")?
+        .inner_html()
+        .trim()
+        .to_owned(),
+    )
+  }
+
+  fn description(&self) -> Result<String> {
+    let content = self
+      .0
+      .root_element()
+      .select_single("div[class='node node-catalog clearfix']")?;
+
+    Ok(
+      content
+        .select_single("div[class='content']")?
+        .select_single("p")?
+        .inner_html()
+        .trim()
+        .split(':')
+        .skip(1)
+        .collect::<Vec<&str>>()
+        .join(" ")
+        .trim()
+        .to_owned(),
+    )
+  }
+
+  fn extract_course_instructors(&self) -> Result<Vec<Instructor>> {
+    let mut instructors = Vec::new();
+
+    let catalog = self.0.root_element().try_select_single(vec![
+      "div[class='node node-catalog clearfix']",
+      "div[class='node node-catalog node-promoted clearfix']",
+    ])?;
+
+    let raw = catalog
+      .select_single("p[class='catalog-terms']")?
+      .inner_html();
+
+    let terms = raw
+      .trim()
+      .split(' ')
+      .skip(1)
+      .filter(|entry| !entry.is_empty())
+      .collect::<Vec<&str>>();
+
+    let mut tokens = catalog
+      .select_single("p[class='catalog-instructors']")?
+      .inner_html()
+      .trim()
+      .split(' ')
+      .skip(1)
+      .collect::<Vec<&str>>()
+      .join(" ");
+
+    terms
+      .join(" ")
+      .split(", ")
+      .map(|term| {
+        (
+          term.split(' ').take(1).collect::<String>(),
+          term.to_string(),
+        )
+      })
+      .for_each(|(term, full_term)| {
+        if tokens.contains(&format!("({term})")) {
+          let split = tokens.split(&format!("({term})")).collect::<Vec<&str>>();
+
+          let inner = split[0]
+            .split(';')
+            .map(|s| {
+              Instructor::default()
+                .set_name(&s.trim().split(", ").collect::<Vec<&str>>())
+                .set_term(&full_term)
+            })
+            .collect::<Vec<Instructor>>();
+
+          if split.len() > 1 {
+            tokens = split[1].trim().to_string();
+          }
+
+          instructors.extend(inner);
+        }
+      });
+
+    Ok(instructors)
+  }
+
+  fn instructors(&self) -> Result<String> {
+    let instructors = self.extract_course_instructors()?;
+
+    let names: Vec<String> = instructors
+      .iter()
+      .map(|instructor| format!("{} ({})", instructor.name, instructor.term))
+      .collect();
+
+    let joined = names.join(", ");
+
+    if names.len() > 1 {
+      let index = joined.rfind(", ").unwrap();
+      Ok(format!("{} and {}", &joined[..index], &joined[index + 2..]))
+    } else {
+      Ok(joined)
+    }
+  }
+}
+
+#[derive(Debug, Default)]
+pub struct Instructor {
+  pub name: String,
+  pub term: String,
+}
+
+impl Instructor {
+  pub fn set_name(self, parts: &[&str]) -> Self {
+    Self {
+      name: format!(
+        "{} {}",
+        parts.get(1).unwrap_or(&""),
+        parts.first().unwrap_or(&"")
+      ),
+      ..self
+    }
+  }
+
+  pub fn set_term(self, term: &str) -> Self {
+    Self {
+      term: term.to_owned(),
+      ..self
+    }
+  }
+}
+
 #[command]
 async fn course(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
   let course_code = args.single::<String>()?;
 
-  // TODO: just call the mcgill.courses public api :)
+  let base_url = format!("https://www.mcgill.ca/study/{MCGILL_TERM}/courses");
+
+  let parsed = Regex::new(r"([a-zA-Z]+)(\d+)")
+    .unwrap()
+    .replace(&course_code, "$1-$2");
+
+  let course_html = CourseHtmlWrapper(Html::parse_fragment(
+    reqwest::get(&format!("{}/{}", base_url, parsed))
+      .await?
+      .text()
+      .await?
+      .as_str(),
+  ));
+
   msg
-    .reply(ctx, format!("You asked for course: {}", course_code))
+    .reply(
+      ctx,
+      format!(
+        "**{}**\n{}\n*Taught by {}*",
+        course_html.title()?,
+        course_html.description()?,
+        course_html.instructors()?,
+      ),
+    )
     .await?;
 
   Ok(())
